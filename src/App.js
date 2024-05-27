7016f12c1fab442f77273a09a4e1d89e4bce19bb
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-27 22:54:05 +0530

    init: inital code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <section>
+            <h1>React JS - Keywords Alert</h1>
+            <main></main>
+        </section>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
